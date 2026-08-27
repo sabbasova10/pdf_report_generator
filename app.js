06ff831283450572b3abfc1db6e74cc67147dec9
sabbasova10 <sugraabbasova4@gmail.com>
2026-08-27 23:30:13 +0400

    html to pdf

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const fs = require("fs");
 const { DatabaseSync } = require('node:sqlite');
 const db = new DatabaseSync('report.db');
+const { chromium } = require("playwright");
 
 const PORT = process.env.PORT;
 app.use(express.json());
@@ -29,26 +30,121 @@ for (const entry of entries) {
     insertStmt.run(entry.title, entry.price_gbp, entry.rating_text, entry.product_url);
 }
 
+async function htmlToPdf(html){
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    
+    await page.setContent(html, { waitUntil: 'networkidle' });
+    
+    const pdf = await page.pdf({
+        format: 'A4',
+        margin: { top: '20mm', bottom: '20mm', left: '15mm', right: '15mm' },
+        printBackground: true,
+    });
+    
+    await browser.close();
+    return pdf;
+}
+
 function getReportData(){
 
     const number = db.prepare("SELECT COUNT(*) AS total FROM books");
     const averagePrice = db.prepare("SELECT AVG(price) AS avg_price FROM books");
     const topFive = db.prepare("SELECT * FROM books ORDER BY price DESC LIMIT 5");
-    const ratingGroups = db.prepare("SELECT rating, COUNT(*) AS count FROM books GROUP BY rating");
+    const allBooks = db.prepare("SELECT * FROM books");
 
     const reportData = {
         books_total: number.get().total,
         average_price: averagePrice.get().avg_price,
         top_5: topFive.all(),
-        rating_groups: ratingGroups.all() 
+        all_books: allBooks.all() 
     };
 
     return reportData;
 
 };
 
-app.get("/health", (req, res) => {
+
+function createHTML(reportData){
+
+    const top5 = reportData.top_5.map(book => `
+        <tr>
+            <td>${book.id}</td>
+            <td>${book.title}</td>
+            <td>£${book.price}</td>
+            <td>${book.rating}</td>
+            <td>${book.url}</td>
+        </tr>
+        `).join('');
+
+    const all = reportData.all_books.map(book => `
+        <tr>
+            <td>${book.id}</td>
+            <td>${book.title}</td>
+            <td>£${book.price}</td>
+            <td>${book.rating}</td>
+            <td>${book.url}</td>
+        </tr>
+        `).join('');
+    
+    const html = `
+        <html>
+            <head>
+                <title>${new Date().toISOString().split('T')[0]}</title>
+                <style>
+                    tr { break-inside: avoid; }
+                </style>
+            </head>
+            <body>
+                <h3>Total number of books: </h3><p>${reportData.books_total}</p>
+                <h3>Average price: </h3><p>${reportData.average_price}</p>
+
+                <h3>Top 5 by price</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Price</th>
+                            <th>Rating</th>
+                            <th>URL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${top5}
+                    </tbody>
+                </table>
+
+            <h3>All books</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Price</th>
+                            <th>Rating</th>
+                            <th>URL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${all}
+                    </tbody>
+                </table>
+
+            </body>
+        </html>
+    `;
+
+    return html;
+}
+
+app.get("/health", async (req, res) => {
     const reportData = getReportData();
+    const html = createHTML(reportData);
+
+    const pdf = await htmlToPdf(html);
+    fs.writeFileSync("test.pdf", pdf);
+
     return res.status(200).json({status: "ok", report: reportData});
 });
 
